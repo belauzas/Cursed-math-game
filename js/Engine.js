c424ef45dfc9bea3a1a490eab7344470d537d788
Rimantas <belauzas@yahoo.com>
2020-08-22 05:35:30 +0300

    Tiles rendering

--- js/Engine.js
+++ js/Engine.js
@@ -1,10 +1,16 @@
 class Engine {
     constructor(params) {
-        this.currentLayer = 'intro';
+        this.introLayerLoadingTime = 1000;
+        this.mapSize = params.map.size;
+        this.mapTiles = params.map.tiles;
+
         this.DOM = document.querySelector('#app');
+
+        this.currentLayer = 'intro';
         this.layersDOM = {
             intro: this.DOM.querySelector('#main_screen'),
-            game: this.DOM.querySelector('#game')
+            game: this.DOM.querySelector('#game'),
+            tiles: this.DOM.querySelector('#game > .tiles')
         };
 
         this.init();
@@ -13,13 +19,32 @@ class Engine {
     init() {
         setTimeout(() => {
             this.changeLayer('game');
-        }, 3000);
+        }, this.introLayerLoadingTime);
+        this.renderMapTiles();
     }
 
     changeLayer(layer) {
         this.layersDOM[this.currentLayer].classList.remove('visible');
         this.currentLayer = layer;
         this.layersDOM[this.currentLayer].classList.add('visible');
+    }
+
+    renderMapTiles() {
+        let HTML = '';
+        const tileTypes = {
+            g: 'platformPack_tile019',
+            y: 'platformPack_tile008',
+            r: 'platformPack_tile020',
+            e: 'platformPack_tile021',
+        }
+        let tileType = '';
+        for (let y = 0; y < this.mapSize.height; y++) {
+            for (let x = 0; x < this.mapSize.width; x++) {
+                tileType = this.mapTiles[y][x];
+                HTML += `<div class="tile" ${tileType !== 'e' ? 'data-exercise= "?"' : ''}><img src="./img/tiles/${tileTypes[tileType]}.png"></div>`;
+            }
+        }
+        this.layersDOM.tiles.innerHTML = HTML;
     }
 }
 
--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 import { Engine } from './Engine.js';
 
 (function () {
-    new Engine();
+    new Engine({
+        map: {
+            size: {
+                width: 9,
+                height: 9
+            },
+            tiles: [
+                ['r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r'],
+                ['r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r'],
+                ['r', 'r', 'y', 'y', 'y', 'y', 'y', 'r', 'r'],
+                ['y', 'y', 'y', 'y', 'y', 'y', 'y', 'y', 'y'],
+                ['y', 'y', 'y', 'y', 'y', 'y', 'y', 'y', 'y'],
+                ['y', 'y', 'g', 'g', 'g', 'g', 'g', 'y', 'y'],
+                ['y', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'y'],
+                ['g', 'g', 'g', 'e', 'e', 'e', 'g', 'g', 'g'],
+                ['g', 'g', 'g', 'e', 'e', 'e', 'g', 'g', 'g']
+            ],
+            playerPosition: { x: 4, y: 8 },
+            trophyPositions: [
+                { x: 2, y: 0 },
+                { x: 6, y: 0 },
+            ]
+        }
+    });
 })();
